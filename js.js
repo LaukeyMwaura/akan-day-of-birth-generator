deca579a8d20f9eade88dd3cc68c4d777d070fc3
LaukeyMwaura <laukeymwaura@gmail.com>
2020-02-13 17:22:28 +0300

    link added to body

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ function output()	{
 	var year = document.getElementById("year").value;
 	var yy = parseInt(year);
 	var cc = (yy-1) / 100+1;
-	var result = parseInt(((cc / 4) - 2 * cc - 1) + (5*yy / 4)) + ((26 * )mm + 1) / 10 )) + dd) % 7;
+	var result = parseInt(((cc / 4) - 2 * cc - 1) + (5*yy / 4)) + ((26 * (mm + 1) / 10 )) + dd)%7;
 	var boy = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 	var girl = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
 	if (document.getElementById("gender").checked)	{
@@ -15,7 +15,7 @@ function output()	{
 	else	{
 		var gender = 'female';
 	}
-	if (mm < 1 || mm > 12 || mm == 2 && dd 29)	{
+	if (mm < 1 || mm > 12 || mm == 2 && dd 29 )	{
 		alert("INVALID MONTH");
 	}
 	else if (dd < 1 || dd > 31)	{
